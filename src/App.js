b1a94344a8121c714b50b706020e34b0f7ff0ee2
claraandrade17 <claraandrade17@gmail.com>
2022-08-23 13:56:48 +0100

    Added dictionary API Integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+     <div className="conatiner">
       <header className="App-header">
         <img src={logo} alt="logo" className="App-logo img-fluid" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='App-footer'>
+        <small>Coded by Clara Mendes</small>
+      </footer>
+     </div>
     </div>
   );
 }
