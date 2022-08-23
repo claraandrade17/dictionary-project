24468368be537f3a5659fbe41ef34ecc1ed4d642
claraandrade17 <claraandrade17@gmail.com>
2022-08-23 13:09:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+
