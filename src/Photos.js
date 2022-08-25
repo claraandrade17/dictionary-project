9778f040f4e3c0888bf3f1614421739a82f0500d
claraandrade17 <claraandrade17@gmail.com>
2022-08-25 22:10:31 +0100

    Final details

--- src/Photos.js
+++ src/Photos.js
@@ -9,8 +9,11 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                <a href={photo.src.original} alt=""
+                target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape}
+                  alt=""
+                  className="img-fluid"/>
                 </a>
               </div>
             );
